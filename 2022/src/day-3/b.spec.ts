e088f8025fb973359ed19608426ba0a82d97f1ce
Grant Steinke <grantjsteinke@gmail.com>
2022-12-06 14:45:44 -0700

    very efficent day 3 part b solution lol

--- 2022/src/day-3/b.spec.ts
+++ 2022/src/day-3/b.spec.ts
@@ -2,6 +2,6 @@ import { day3b } from './b';
 
 describe('Puzzle 3 b', () => {
   test('given example', async () => {
-    expect(await day3b('data/day-3/b-test.txt')).toBe(0);
+    expect(await day3b('data/day-3/b-test.txt')).toBe(70);
   });
 });
--- 2022/src/day-3/b.ts
+++ 2022/src/day-3/b.ts
@@ -3,7 +3,75 @@ import chalk from 'chalk';
 
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  // const duplicates = [];
+  let total = 0;
+
+  const values = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+    A: 27,
+    B: 28,
+    C: 29,
+    D: 30,
+    E: 31,
+    F: 32,
+    G: 33,
+    H: 34,
+    I: 35,
+    J: 36,
+    K: 37,
+    L: 38,
+    M: 39,
+    N: 40,
+    O: 41,
+    P: 42,
+    Q: 43,
+    R: 44,
+    S: 45,
+    T: 46,
+    U: 47,
+    V: 48,
+    W: 49,
+    X: 50,
+    Y: 51,
+    Z: 52,
+  };
+
+  for (let i = 0; i < data.length; i += 3) {
+    for (const letter of data[i]) {
+      if (data[i + 1].includes(letter) && data[i + 2].includes(letter)) {
+        total = total + values[letter];
+        break;
+      }
+    }
+  }
+
+  return total;
 }
 
 // don't change below this line
