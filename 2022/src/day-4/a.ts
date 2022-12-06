9fc40b3c248c078a8df694808c9d6908ec98a083
Grant Steinke <grantjsteinke@gmail.com>
2022-12-06 15:21:49 -0700

    day 4 part a solution

--- 2022/src/day-4/a.ts
+++ 2022/src/day-4/a.ts
@@ -3,7 +3,22 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let count = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const splitNumbers = data[i].match(/(?:\d+\.)?\d+/g);
+
+    if (
+      (Number(splitNumbers[0]) <= Number(splitNumbers[2]) &&
+        Number(splitNumbers[1]) >= Number(splitNumbers[3])) ||
+      (Number(splitNumbers[0]) >= Number(splitNumbers[2]) &&
+        Number(splitNumbers[1]) <= Number(splitNumbers[3]))
+    ) {
+      count = count + 1;
+    }
+  }
+
+  return count;
 }
 
 // don't change below this line
