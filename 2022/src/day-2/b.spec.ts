ff14168f95ee74c78397f102209484924d277979
Grant Steinke <grantjsteinke@gmail.com>
2022-12-06 13:44:51 -0700

    day 2 part b

--- 2022/src/day-2/b.spec.ts
+++ 2022/src/day-2/b.spec.ts
@@ -2,6 +2,6 @@ import { day2b } from './b';
 
 describe('Puzzle 2 b', () => {
   test('given example', async () => {
-    expect(await day2b('data/day-2/b-test.txt')).toBe(0);
+    expect(await day2b('data/day-2/b-test.txt')).toBe(12);
   });
 });
--- 2022/src/day-2/b.ts
+++ 2022/src/day-2/b.ts
@@ -3,7 +3,50 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let total = 0;
+
+  for (var i = 0; i < data.length; i++) {
+    let elfLetter = data[i][0];
+    let yourLetter = data[i][data[i].length - 1];
+
+    if (elfLetter === 'A') {
+      //1
+      if (yourLetter === 'X') {
+        total = total + 3; //0 + 3
+      }
+      if (yourLetter === 'Y') {
+        total = total + 4; //3 + 1
+      }
+      if (yourLetter === 'Z') {
+        total = total + 8; //6 + 2
+      }
+    }
+    if (elfLetter === 'B') {
+      if (yourLetter === 'X') {
+        total = total + 1; //0 + 1
+      }
+      if (yourLetter === 'Y') {
+        total = total + 5; //3 + 2
+      }
+      if (yourLetter === 'Z') {
+        total = total + 9; //6 + 3
+      }
+    }
+    if (elfLetter === 'C') {
+      if (yourLetter === 'X') {
+        total = total + 2; //0 + 2
+      }
+      if (yourLetter === 'Y') {
+        total = total + 6; //3 + 3
+      }
+      if (yourLetter === 'Z') {
+        total = total + 7; //6 + 1
+      }
+    }
+  }
+
+  return total;
 }
 
 // don't change below this line
